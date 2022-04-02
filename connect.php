64eeaa47bf3c6e10aeeeebf5cc0a506a69657f6c
Demithier01 <102402386+Demithier01@users.noreply.github.com>
2022-04-02 20:09:35 +0700

    connect

---
+++ connect.php
@@ -0,0 +1,16 @@
+<?php
+  $servername = "localhost";
+  $username = "root";
+  $password = "";
+  $database = "kfc";
+  
+  // Create connection
+  $conn = new mysqli($servername, $username, $password, $database);
+
+    if (mysqli_connect_errno()) {
+        echo "Failed to connect to MySQL: " . mysqli_connect_error();
+        exit();
+    }
+    //echo"Connected successfully";
+   
+?>
